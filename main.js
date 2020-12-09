b60f5b5cc0e2b27f1896297fc525045861a26ebf
Justin49 <justin.bell@hotmail.fr>
2020-12-09 22:04:48 +0100

    création de la logique du jeu

--- main.js
+++ main.js
@@ -56,6 +56,8 @@ function dessinerTableau2D(grille) {
         for(var j = 0; j < grille[i].length; j++) {
 
             // je conditionne pour dire si ma cellule est morte ou vivante
+
+            // si elle est morte alors on dessine la cellule en noire
             if(grille[i][j] === 0) {
 
                 contexte.beginPath();
@@ -66,6 +68,7 @@ function dessinerTableau2D(grille) {
 
             }
 
+            // si elle est vivante on dessine la cellule en blanc
             else if(grille[i][j] === 1) {
 
                 contexte.beginPath();
@@ -80,5 +83,100 @@ function dessinerTableau2D(grille) {
     
 }
 
-dessinerTableau2D(grille);
+// fonction qui va créer la logique du jeu
+function logiqueDuJeu(grille) {
+
+    // je créer un nouveau tableau qui va être la génération suivante
+    const nouveauTableau = creerTableau2D();
+
+    // je parcours mon nouveau tableau
+    for(var i = 0; i < grille.length; i++) {
+
+        for(var j = 0; j < grille[i].length; j++) {
+
+            // j'affecte ma grille dans mon nouveau tableau créer précedemment
+            nouveauTableau[i][j] = grille[i][j];
+
+        }
+    }
+
+    // je parcours mon nouveau tableau à 2 dimensions, pour selectionner mon pixel
+    for(var i = 0; i < nouveauTableau.length; i++) {
+
+        for(var j = 0; j < nouveauTableau[i].length; j++) {
+
+            let nombreDeVoisin = 0;
+
+            // pour chaque indice de mon tableau à 2 dimensions, il faut que je compte combien une cellule à de voisines vivante
+            for(var k = -1; k <= 1; k++) {
+
+                for(var l = -1; l <= 1; l++) {
+
+                    // on  vérifie si l'indice qu'on vérifie n'est pas son propre voisin, si il l'est il s'arrête ici sinon il continue
+                    if(k === 0 && l === 0) {
+
+                        continue;
+                    }
+
+                    // constante qui servent à savoir si la cellule est en dehors du caneva (zone que l'on ne parcours pas)
+                    const x_cell = k + i;
+                    const y_cell = l + j;
+
+                    if (x_cell >= 0 && y_cell >= 0 && x_cell < grille.length && y_cell < grille.length) {
+
+                        // j'incrémente le nombre de voisin de la cellule
+                        nombreDeVoisin++;
+                    }
+                }
+            }
+
+            // par rapport au nombre de voisin, on décide si une cellule va vivre ou mourrir
+
+            // si la cellule du tableau est morte et qu'elle à 3 voisins
+            if(nouveauTableau[k][l] === 0 && nombreDeVoisin === 3) {
+
+                // la cellule va vivre
+                nouveauTableau[k][l] = 1;
+            }
+
+            // si la cellule du tableau est vivante et qu'elle à strictement moins de 2 voisins
+            else if(nouveauTableau[k][l] === 1 && nombreDeVoisin < 2) {
+
+                // la cellule va mourrir
+                nouveauTableau[k][l] = 0;
+            }
+
+            // si la cellule du tableau est vivante et qu'elle à strictement plus de 3 voisins
+            else if(nouveauTableau[k][l] === 1 && nombreDeVoisin > 3) {
+
+                // la cellule va mourrir
+                nouveauTableau[k][l] = 0;
+            }
+
+        }
+
+    }
+
+    return nouveauTableau;
+
+}
+
+// création de la boucle principale du jeu
+requestAnimationFrame(bouclePrincipale);
+
+function bouclePrincipale() {
+
+    // dessine mon tableau
+    dessinerTableau2D(grille);
+
+    // j'appelle la fonction qui va contenir la logique du jeu et donc la nouvelle grille
+    grille = logiqueDuJeu(grille);
+ 
+  requestAnimationFrame(bouclePrincipale);
+  console.log("je passe par ici");
+}
+
+
+
+
 
